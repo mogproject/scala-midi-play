e9ea106a20b0ca82d773d6981bbd92d18aa5fe98
Yosuke Mizutani <mogproj@gmail.com>
2015-08-09 01:16:56 +0900

    refactored

--- scripts/concurrency_sonification.scala
+++ scripts/concurrency_sonification.scala
@@ -7,33 +7,35 @@ import scala.util.Random
 val random = new Random(12345)
 val rcvr = MidiSystem.getReceiver()
 
-def changeProgram(program: Int) = {
+
+def sendMessage(event: Int, channel: Int, data1: Int, data2: Int): Unit = {
   val msg = new ShortMessage
-  msg.setMessage(PROGRAM_CHANGE, 0, program, 0)
+  msg.setMessage(event, channel, data1, data2)
   rcvr.send(msg, -1)
 }
 
-def noteOn(key: Int, duration: Long) = {
-  val msg = new ShortMessage
 
-  msg.setMessage(NOTE_ON, 0, key, 93)
-  rcvr.send(msg, -1)
-
+def noteOn(key: Int, duration: Long): Unit = {
+  sendMessage(NOTE_ON, 0, key, 93)
   Thread.sleep(duration)
-
-  msg.setMessage(NOTE_OFF, 0, key, 0)
-  rcvr.send(msg, -1)
+  sendMessage(NOTE_OFF, 0, key, 0)
 }
 
 
 def f(x: Int): Unit = {
   println(s"${x}: start")
-  noteOn(48 + x, 500L + random.nextInt(2000))
+  noteOn(48 + x, 300L * (random.nextInt(8) + 1))
   println(s"${x}: end")
 }
 
 // Change the program to string ensamble.
-changeProgram(49)
+sendMessage(PROGRAM_CHANGE, 0, 49, 0)
 
+println("### (21 to 30).foreach(f)")
+(21 to 30).foreach(f)
+Thread.sleep(2000L)
+
+println("### (1 to 30).par.foreach(f)")
 (1 to 30).par.foreach(f)
+Thread.sleep(2000L)
 
